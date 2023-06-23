e262d524ab742f338b6d5808b6df7ea89237feba
kareemalrefai <kareemalrefai@gmail.com>
2023-06-23 16:45:45 +0300

    update

---
+++ fun.js
@@ -0,0 +1,147 @@
+// function multiply(params) {
+//     console.log("4*8")
+// }
+
+// multiply()
+
+
+
+
+//----------------------------------------------------------------------------------------------
+
+// function multiply(firstnumber , secondnumber) {
+//     console.log(firstnumber*secondnumber)            //(+ , - , /,%)
+
+// }
+
+// multiply(5,8)
+// multiply(9,12)
+// multiply(3,8)
+// multiply()       //---Nan
+
+//------------------------------------------------------------------------------------------------
+
+
+
+// function Maths(firstnumber,secondnumber) {
+//     for(let i=firstnumber ; i<secondnumber ; i++ ){
+//         if(i%2==1){
+//             console.log("this is a odd number"+" "+i)
+
+//         }
+
+//     }
+
+// }
+// Maths(5,10)
+// Maths(40,50)
+
+
+
+//-------------------------------------- //prompt //-----------------------------------------------------
+
+
+// let theusername = prompt("Please Enter Your Name")
+// function EX(firstnumber, secondnumber) {
+
+//     for (let i = firstnumber; i < secondnumber; i++) {
+
+//         if (i % 2 == 0) {
+//             console.log(i + " " + theusername)
+//         }
+//     }
+
+// }
+
+// EX(0, 20)
+
+
+
+
+// let theusername = prompt("Enter your name")
+
+// function EXA(firstnumber, secondnumber) {
+//     for (let i = firstnumber ; i < secondnumber ; i++) {
+
+//         if (i % 2 == 0) {
+//             console.log(i + " " + theusername)
+
+//         }
+
+//     }
+// }
+
+// EXA(0, 10)
+
+
+//--------------------------------------// Array // ------------------------------------------------------
+
+
+
+
+// let Mystudent = ["ahmad","ali","khaled","majed"]
+// let MystudentMarks = [40 , 59 , 84 ,93]
+
+// function markCalculate(username , marks) {
+//     for(let i=0 ; i< Mystudent.length ; i++){
+//         console.log(Mystudent[i])
+
+
+//     }
+
+//     for(let k=0 ; k<MystudentMarks.length ; k++){
+//         console.log(MystudentMarks[k])
+//     }
+    
+// }
+
+// markCalculate()
+
+
+
+
+//------------------------------------ //HomeWork // --------------------------------------------------------------
+
+
+// let mystudants =["ali", "khaled" , "omar" , "laith"]
+// let mystudantsMarks= [48 , 68 , 93 , 35]
+
+
+// function claculatemarks(params) {
+
+// for(let i=0 ; i<mystudants.length ; i++)
+// {
+  
+//     console.log(mystudants[i]+ " " + mystudantsMarks[i] + (mystudantsMarks[i]>50 ? " "+"Failed"+" ":" "+"Pass"));
+
+// }
+
+// }
+
+// claculatemarks()
+
+
+
+
+// let names = ["khaled", "ali", "omar", "ahmad"];
+// let marks = [38, 44, 90, 73];
+
+// for (let i = 0; i < names.length; i++) {
+//   console.log(names[i] + ": " + marks[i] + (marks[i] > 50 ? " ناجح" : " راسب"));
+// }
+
+
+
+
+// let Sname =["mnaar" , "shadi" , "khaldon" , "jameela"]
+// let Smark =[80 , 45 , 59 , 35]
+
+// function myclass(params) {
+//     for(let i=0 ; i<Smark.length; i++){
+//         console.log(Sname[i] + " " + Smark[i]+ " " + (Smark[i]>50? " pass" + " " : + " " +"Fail"))
+
+//     }
+    
+// }
+
+// myclass()
